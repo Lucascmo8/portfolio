54b4025f43e7469b474444fa7fc11cdf007a659d
Lucas Costa <103662624+Lucascmo8@users.noreply.github.com>
2023-01-05 15:10:21 -0300

    adicionando a função de keypress
    - adicionado keypress para as teclas
    - e mundança em alguns IDs e name

--- calculadora/script.js
+++ calculadora/script.js
@@ -1,3 +1,27 @@
+const calculo = document.getElementById("calculo")
+
 function displaynum(n1){
-    calculator.text1.value = calculator.text1.value+n1;
+    calculo.value = calculo.value+n1;
+
+    transformar(calculo.value)
+}
+
+
+const caracteresEspeciais = ["+","-","*","/","."]
+const teclasDeResultado = ["Enter","="]
+
+function digitar(event){
+        if(event.key >= 0 || event.key <=9){
+            displaynum(event.key)
+        }
+        if(caracteresEspeciais.includes(event.key) == true){
+            displaynum(event.key)
+        }
+        if(teclasDeResultado.includes(event.key) == true){
+            calculo.value=eval(calculo.value)
+        }
+}
+
+function limparCalculo(){
+    calculator.calculo.value = ""
 }
